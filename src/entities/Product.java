44c90b21a87c13658b2b270b10897d8262606c5f
Hetel Santos <hetel.santos@gmail.com>
2019-05-11 16:07:36 -0300

    First version

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -28,4 +28,11 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + String.format("%.2f", price) + "]";
+	}
+	
+	
 }
